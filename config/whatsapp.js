b83ef093b2acecfe4cef4aba0eae63ce5e628dde
jordy <jordyvigo@hotmail.com>
2025-04-12 15:42:37 -0500

    wsp2

--- config/whatsapp.js
+++ config/whatsapp.js
@@ -3,6 +3,7 @@ const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const QRCode = require('qrcode');
 const path = require('path');
 
+// Configuración del cliente
 const client = new Client({
   authStrategy: new LocalAuth({ clientId: 'cardroid-bot' }),
   puppeteer: {
@@ -18,6 +19,7 @@ const client = new Client({
   }
 });
 
+// Manejo del evento QR
 client.on('qr', async (qrCode) => {
   console.debug('QR recibido.');
   try {
@@ -29,16 +31,23 @@ client.on('qr', async (qrCode) => {
   }
 });
 
+// Evento cuando el cliente está listo
 client.on('ready', () => {
   console.debug('WhatsApp Bot listo para recibir mensajes!');
 });
 
+// Captura de errores de autenticación
 client.on('auth_failure', msg => console.error('Error de autenticación:', msg));
 
+// Inicialización del cliente
 client.initialize();
 
 // Función para enviar un mensaje de texto
 async function sendWhatsAppMessage(to, message) {
+  // Validamos que el cliente esté listo (client.info se establece cuando el cliente ha iniciado sesión correctamente)
+  if (!client.info) {
+    throw new Error("El cliente no está listo, espere a que se autentique.");
+  }
   // Asegurarse de que el número tenga el sufijo '@c.us'
   const formattedTo = to.includes('@c.us') ? to : `${to}@c.us`;
   return client.sendMessage(formattedTo, message);
@@ -46,13 +55,16 @@ async function sendWhatsAppMessage(to, message) {
 
 // Función para enviar una imagen con caption
 async function sendWhatsAppMedia(to, imageUrl, caption) {
+  if (!client.info) {
+    throw new Error("El cliente no está listo, espere a que se autentique.");
+  }
   const formattedTo = to.includes('@c.us') ? to : `${to}@c.us`;
-  // Descarga el medio desde la URL y envíalo
+  // Descarga el medio desde la URL y luego lo envía
   const media = await MessageMedia.fromUrl(imageUrl);
   return client.sendMessage(formattedTo, media, { caption });
 }
 
-// Exportamos el client y las funciones de envío para que puedan ser usadas en otros módulos
+// Exportamos el cliente y las funciones para que puedan ser usadas en otros módulos
 module.exports = {
   client,
   sendWhatsAppMessage,
